8a5936d4abb89fd90017f08cdee189f692f9b06d
CodeZeroJake <ahmettalhakatas123@gmail.com>
2025-08-12 13:18:10 +0200

    continued from where last left, finished project, still have to do live server testing

--- index.js
+++ index.js
@@ -9,22 +9,51 @@ function myFunc(){
     console.log(el2.checked);
     console.log(el3.value);
 
-    let el4val = getSelectVal(el4)
-    console.log(el4val)
+    let el4val = getSelectVal(el4);
+    console.log(el4val);
 
+    let el1val = el1.value;
+    let el2val = el2.checked;
+
+    if(el2val == true){
+        el2val = "checked";
+    } else {
+        el2val = "unchecked";
+    }
+
+    let el3val = el3.value;
+
+    let values = setValues(el1val, el2val, el3val, el4val);
+    console.log(values)
 }
 
 function getSelectVal(el4){
-    let x = el4.length;
-    const myArray[] = el4;
-    for (let i = 0; i < x; i++){
-        if myArray[i] = "true"; 
+    for(let i = 0; i < el4.length; i++){
+        if(el4.options[i].selected == true){
+            el4 = el4.options[i].value;
+        }
     }
-
-    return ""
-
+    return el4;
 }
 
+const storedValues = [];
+
+function setValues(el1val, el2val, el3val, el4val){
+    const valuesArray = [el1val, el2val, el3val, el4val];
+    for(let i = 0; i < valuesArray.length; i++){
+        storedValues[i] = valuesArray[i];
+    }
+
+    console.log(storedValues);
+
+    return valuesArray;
+}
 
 function myOutput(){
+    document.getElementById("GetVal").innerHTML = 
+    "Textbox : " + storedValues[0] + "<br>" +
+    "Checkbox : " + storedValues[1] + "<br>" +
+    "Textarea : " + storedValues[2] + "<br>" +
+    "Select : " + storedValues[3];
+
 }
